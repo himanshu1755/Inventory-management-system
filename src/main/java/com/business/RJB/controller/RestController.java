b25ee16fc6aa8e209110f8e270e7a95c4252a60f
Himanshu <38362189+himanshu1755@users.noreply.github.com>
2023-08-23 13:58:54 +0530

    logger added and controller changed

--- src/main/java/com/business/RJB/controller/RestController.java
+++ src/main/java/com/business/RJB/controller/RestController.java
@@ -1,7 +1,9 @@
 package com.business.RJB.controller;
 
 import lombok.CustomLog;
+import lombok.extern.log4j.Log4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +13,7 @@ import reactor.core.publisher.Mono;
 
 @CrossOrigin
 @Controller
-
+@CustomLog
 public class RestController {
 
 
@@ -25,5 +27,13 @@ public class RestController {
     public String page(Model model) throws Exception {
         System.out.println("hi");
         return "login/index";
+    }
+
+    @PostMapping(value = "/addMerchant", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> addAppConfig(@RequestBody String merchantData) {
+        log.info("merchantData --- {}", merchantData);
+//        Response response = adminService.addAppConfigData(appConfig);
+        return ResponseEntity.accepted().body("");
+
     }
 }
